f623c12f3b87b4e4f264f21adde41d19a99b27f2
Steven Gentens <stg@foreach.be>
2018-09-14 08:52:53 +0200

    Simplified DocumentImporter

--- src/main/java/com/foreach/demo/dfm/application/installers/DocumentImporter.java
+++ src/main/java/com/foreach/demo/dfm/application/installers/DocumentImporter.java
@@ -3,17 +3,15 @@ package com.foreach.demo.dfm.application.installers;
 import com.foreach.across.core.annotations.Installer;
 import com.foreach.across.core.annotations.InstallerMethod;
 import com.foreach.across.core.installers.InstallerPhase;
-import com.foreach.across.modules.dynamicforms.domain.dataset.DynamicDefinitionDataSet;
-import com.foreach.across.modules.dynamicforms.domain.dataset.DynamicDefinitionDataSetRepository;
 import com.foreach.across.modules.dynamicforms.domain.definition.DynamicDefinition;
-import com.foreach.across.modules.dynamicforms.domain.definition.DynamicDefinitionRepository;
+import com.foreach.across.modules.dynamicforms.domain.definition.DynamicDefinitionId;
+import com.foreach.across.modules.dynamicforms.domain.definition.DynamicDefinitionService;
 import com.foreach.across.modules.dynamicforms.domain.document.DynamicDocumentService;
 import com.foreach.across.modules.dynamicforms.domain.document.DynamicDocumentWorkspace;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.yaml.snakeyaml.Yaml;
@@ -25,29 +23,26 @@ import java.util.Map;
 @Installer(name = "document-importer", description = "Creates the initial documents", phase = InstallerPhase.AfterContextBootstrap, version = 1)
 @Slf4j
 @Order(2000)
-public class DocumentImporter {
-    private final DynamicDocumentService documentService;
-    private final DynamicDefinitionRepository definitionRepository;
-    private final DynamicDefinitionDataSetRepository dataSetRepository;
+public class DocumentImporter
+{
+	private final DynamicDocumentService documentService;
+	private final DynamicDefinitionService definitionService;
 
-    @Value("classpath:demo-dfm/training-samples.yml")
-    private Resource trainings;
+	@Value("classpath:demo-dfm/training-samples.yml")
+	private Resource trainings;
 
-    @InstallerMethod
-    @SneakyThrows
-    @SuppressWarnings("unchecked")
-    public void importDocuments() {
-        DynamicDefinitionDataSet dataSet = dataSetRepository.findOneByName(DefinitionInstaller.DEMO_DATASET);
-        List<DynamicDefinition> definitions = definitionRepository.findByDataSetAndName(dataSet, DefinitionInstaller.TRAINING_DEFINITION_NAME );
-        if( definitions.size() > 0 ) {
-            Yaml yaml = new Yaml();
-            List<Map<String, Object>> raw = yaml.loadAs(trainings.getInputStream(), List.class);
-            raw.forEach( training -> {
-                DynamicDocumentWorkspace workspace = documentService.createDocumentWorkspace(definitions.get(0));
-                workspace.setDocumentName((String) training.get("documentName"));
-                workspace.importFields((Map<String, Object>) training.get("fields"));
-                workspace.save();
-            });
-        }
-    }
+	@InstallerMethod
+	@SneakyThrows
+	@SuppressWarnings("unchecked")
+	public void importDocuments() {
+		DynamicDefinition definition = definitionService.findDefinition( DynamicDefinitionId.fromString( "trainings:training" ) );
+		Yaml yaml = new Yaml();
+		List<Map<String, Object>> raw = yaml.loadAs( trainings.getInputStream(), List.class );
+		raw.forEach( training -> {
+			DynamicDocumentWorkspace workspace = documentService.createDocumentWorkspace( definition );
+			workspace.setDocumentName( (String) training.get( "documentName" ) );
+			workspace.importFields( (Map<String, Object>) training.get( "fields" ) );
+			workspace.save();
+		} );
+	}
 }
